632483d8f545eae2231936d59fe97c110c3a8292
Vibhor-Kolte <vibhorkolte@gmail.com>
2023-10-08 00:00:09 +0530

    [Dev-002] - Declared score, startScreen, gameArea components

---
+++ main.js
@@ -0,0 +1,7 @@
+const score = document.querySelector('.score');
+const startScreen = document.querySelector('.startScreen');
+const gameArea = document.querySelector('.gameArea');
+
+console.log(score);
+console.log(startScreen);
+console.log(gameArea);
